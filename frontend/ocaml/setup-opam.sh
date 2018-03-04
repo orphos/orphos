2ae65fcf7793689a2fb9101bd5d7721c8f6161d8
Takezoe,Tomoaki <tomoaki3478@res.ac>
2018-03-04 21:57:10 +0900

    Add setup-opam.sh

---
+++ frontend/ocaml/setup-opam.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -uxve
+opam switch -y orphos --alias-of 4.06.0
+eval $(opam config --shell=bash env)
+
+# dependency packages
+opam pin -y add ocamlfind 1.7.3-1
+opam pin -y add num 1.1
+opam pin -y add ocamlbuild 0.12.0
+opam pin -y add batteries 2.8.0
+opam pin -y add jbuilder 1.0+beta18
+opam pin -y add menhir 20171222
+opam pin -y add ounit 2.0.7
+
+# development tools
+opam pin -y add cppo 1.6.4
+opam pin -y add easy-format 1.3.1
+opam pin -y add biniou 1.2.0
+opam pin -y add yojson 1.4.1
+opam pin -y add merlin 3.0.5
+
+echo 'Run `eval $(opam config env)` before starting to develop ocaml-orphos!'
+
