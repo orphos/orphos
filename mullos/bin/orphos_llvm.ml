259a5152357614836ea8c77d6e4a786f6813cc7d
Takezoe,Tomoaki <tomoaki3478@res.ac>
2018-12-02 17:00:08 +0900

    mullos:Use Exn.protect to dispose LLVM objects

--- mullos/bin/orphos_llvm.ml
+++ mullos/bin/orphos_llvm.ml
@@ -2,23 +2,22 @@
  *
  * SPDX-Identifier: LGPL-3.0-or-later
  *)
+open Base
 open Llvm
 
 let hello_llvm () = begin
   let c = create_context() in
-  try
+  Exn.protect ~f:(fun () ->
     let m = create_module c "hello module" in
-    try
+    Exn.protect ~f:(fun () ->
       let i32_type = i32_type c in
       let f_type = function_type i32_type [||] in
       let f = define_function "get_answer" f_type m in
       let builder = builder_at_end c (entry_block f) in
       let _ = build_ret (const_int i32_type 42) builder in
       string_of_llmodule m
-    with e ->
-      dispose_module m;
-      raise e
-  with e ->
-    dispose_context c;
-    raise e
+    ) ~finally:(fun () ->
+      dispose_module m
+    )
+  ) ~finally:(fun() -> dispose_context c)
 end
